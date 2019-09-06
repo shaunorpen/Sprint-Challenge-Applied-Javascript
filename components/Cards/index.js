e74d3798b2c3a70ad878dc64d965e86bf520a47c
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-09-06 09:21:14 +0100

    Add cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,41 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        Object.keys(response.data.articles).map(articleType => {
+            response.data.articles[articleType].forEach(article => {
+                document.querySelector('.cards-container').appendChild(createCard(article));
+            });
+        });
+    })
+    .catch(error => {
+        console.log('Error: ', error);
+    });
+
+function createCard (data) {
+    const divCard = document.createElement('div');
+    const divHeadline = document.createElement('div');
+    const divAuthor = document.createElement('div');
+    const divImgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const spanBy = document.createElement('div');
+
+    divCard.classList.add('card');
+    divHeadline.classList.add('headline');
+    divAuthor.classList.add('author');
+    divImgContainer.classList.add('img-container');
+
+    divHeadline.textContent = data.headline;
+    img.src = data.authorPhoto;
+    spanBy.textContent = 'By ' + data.authorName;
+
+    divCard.appendChild(divHeadline);
+    divCard.appendChild(divAuthor);
+    divAuthor.appendChild(divImgContainer);
+    divImgContainer.appendChild(img);
+    divAuthor.appendChild(spanBy);
+
+    return divCard;
+}
