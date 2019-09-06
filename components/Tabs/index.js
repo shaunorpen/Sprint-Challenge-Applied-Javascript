0da72928ded282c8c7cea969ae63c292b6d70e18
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-09-06 10:07:09 +0100

    Add all tab plus click eventlistener to all tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -10,6 +10,7 @@
 
 axios.get('https://lambda-times-backend.herokuapp.com/topics')
     .then(response => {
+        document.querySelector('.tabs .topics').appendChild(createTab('All'));
         response.data.topics.forEach(topic => {
             document.querySelector('.tabs .topics').appendChild(createTab(topic));
         })
@@ -22,5 +23,13 @@ function createTab (tabName) {
     const divTab = document.createElement('div');
     divTab.classList.add('tab');
     divTab.textContent = tabName;
+    divTab.addEventListener('click', toggleActive);
     return divTab;
+}
+
+function toggleActive (e) {
+    document.querySelectorAll('.tab').forEach(tab => {
+        tab.classList.remove('active-tab');
+    })
+    e.target.classList.toggle('active-tab');
 }
